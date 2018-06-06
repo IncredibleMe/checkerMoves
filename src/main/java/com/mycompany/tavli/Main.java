67ed0217449d55f6524040f6c60f3eee95d1545d
jack <immortalmors@hotmail.com>
2018-06-06 18:46:19 +0300

    no doubles

--- src/main/java/com/mycompany/tavli/Main.java
+++ src/main/java/com/mycompany/tavli/Main.java
@@ -1,15 +1,21 @@
 package com.mycompany.tavli;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
     private static int[] board = {2, 0, 0, 0, 0, -5, 0, -3, 0, 0, 0, 5, -5, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, -2, 0, 0, 0, 0};
 
     public static void main(String[] args) {
-        printTable();
+        printTable("");
         int d1 = 6;
-        int d2 = 6;
+        int d2 = 2;
 
         move(d1, d2);
 
@@ -40,6 +46,8 @@ public class Main {
     }
 
     static void move(int d1, int d2) {
+        String move = "";
+        String move2 = "";
         int[] board2;
         board2 = new int[28];
         board2 = Arrays.copyOf(board, 28);
@@ -50,6 +58,7 @@ public class Main {
             if (board[d1 - 1] >= 0) {
                 board[d1 - 1]++;
                 board[26]--;
+                
                 board3 = Arrays.copyOf(board, 28);
                 for (int j = 0; j < 24; j++) {
                     if (board[j] > 0) {
@@ -58,18 +67,18 @@ public class Main {
                                 if ((j + d2) > 23) {
                                     board[24]++;
                                     board[j]--;
-                                    printTable();
+                                    printTable(move);
                                     board = Arrays.copyOf(board3, 28);
                                 } else {
                                     board[j + d2]++;
                                     board[j]--;
-                                    printTable();
+                                    printTable(move);
                                     board = Arrays.copyOf(board3, 28);
                                 }
                             } else if ((j + d2) <= 23) {
                                 board[j]--;
                                 board[j + d2]++;
-                                printTable();
+                                printTable(move);
                                 board = Arrays.copyOf(board3, 28);
                             }
                         }
@@ -91,18 +100,18 @@ public class Main {
                                 if ((j + d1) > 23) {
                                     board[24]++;
                                     board[j]--;
-                                    printTable();
+                                    printTable(move);
                                     board = Arrays.copyOf(board3, 28);
                                 } else {
                                     board[j + d1]++;
                                     board[j]--;
-                                    printTable();
+                                    printTable(move);
                                     board = Arrays.copyOf(board3, 28);
                                 }
                             } else if ((j + d1) <= 23) {
                                 board[j]--;
                                 board[j + d1]++;
-                                printTable();
+                                printTable(move);
                                 board = Arrays.copyOf(board3, 28);
                             }
                         }
@@ -115,12 +124,13 @@ public class Main {
         } else {
 
             for (int i = 0; i < 24; i++) {
-
+                
                 if (board[i] > 0) {
 
                     if (board[i + d1] >= 0 && i + d1 < 24) {
                         board[i]--;
                         board[i + d1]++;
+                        move2 = "" + i + "->" + (i+d1) + " and ";
                         board3 = Arrays.copyOf(board, 28);
 
                         for (int j = 0; j < 24; j++) {
@@ -130,20 +140,23 @@ public class Main {
                                     if ((j + d2) > 23) {
                                         board[24]++;
                                         board[j]--;
-                                        printTable();
+                                        move = move2 +  j + " -> " + "24";
+                                        printTable(move);
                                         board = Arrays.copyOf(board3, 28);
                                     } else {
-                                        
+
                                         board[j + d2]++;
                                         board[j]--;
-                                        printTable();
+                                        move = move2 +  j + " -> " + (j+d2);
+                                        printTable(move);
                                         board = Arrays.copyOf(board3, 28);
                                     }
                                 } else if ((j + d2) <= 23) {
                                     if (board[j + d2] >= 0) {
                                         board[j]--;
                                         board[j + d2]++;
-                                        printTable();
+                                        move = move2 +  j + " -> " + (j+d2);
+                                        printTable(move);
                                         board = Arrays.copyOf(board3, 28);
                                     }
                                 }
@@ -157,7 +170,8 @@ public class Main {
         }
     }
 
-    static void printTable() {
+    static void printTable(String move) {
+        
         int[] board2;
         board2 = new int[28];
         board2 = Arrays.copyOf(board, 28);
@@ -231,6 +245,41 @@ public class Main {
 
         System.out.println("=======================================");
         System.out.println("");
+        try {
+            insertToDB(move);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+    private static void insertToDB(String move) throws ClassNotFoundException, SQLException {
+        String dbCon = "jdbc:mysql://195.251.162.129:3306/giannakaras";
+
+        //STEP 2: Register JDBC driver
+        Class.forName("com.mysql.jdbc.Driver");
+
+        //STEP 3: Open a connection
+        System.out.println("Connecting to a selected database...");
+        Connection conn = DriverManager.getConnection(dbCon, "giannakaras", "giannakaras");
+        System.out.println("Connected database successfully...");
+
+        Statement stmt = null;
+        //STEP 4: Execute a query
+        System.out.println("Inserting records into the table...");
+        stmt = conn.createStatement();
+
+        String sql = "INSERT INTO nextmove VALUES (";
+                for (int i=0; i<27; i++){
+                sql+= board[i] + ", ";
+                
+                }
+                sql+= board[27] + ", '" + move + "');";
+                System.out.println(sql);
+        stmt.executeUpdate(sql);
     }
 
 }
